da6d7283b4251cc7092a1d829b11d9504d8fd6a7
Hawi Kebebew <Hawi.Kebebew@safaricom.et>
2025-05-21 15:02:43 +0300

    feat: basic Streamlit UI

--- app/utils.py
+++ app/utils.py
@@ -2,13 +2,13 @@
 import pandas as pd
 
 def load_all_data():
-    benin = pd.read_csv('data/benin_clean.csv')
+    benin = pd.read_csv('data/benin-malanville_clean')
     benin["Country"] = "Benin"
 
-    sierra = pd.read_csv('data/sierra_leone_clean.csv')
+    sierra = pd.read_csv('data/sierraleone-bumbuna_clean')
     sierra["Country"] = "Sierra Leone"
 
-    togo = pd.read_csv('data/togo_clean.csv')
+    togo = pd.read_csv('data/togo-dapaong_qc_clean')
     togo["Country"] = "Togo"
 
     return pd.concat([benin, sierra, togo], ignore_index=True)
